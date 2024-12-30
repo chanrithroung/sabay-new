74bd24bfa0d8e3a8b8111be1f2c630026e904c76
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-12-30 16:50:12 +0700

    form

--- script.js
+++
@@ -1,9 +0,0 @@
-const sidebar = document.querySelector('.sidebar');
-
-const btnToggle = document.querySelector('.btn-toggle');
-
-let isShow = false;
-btnToggle.addEventListener('click', function(){
-    sidebar.style.width = !isShow ?  '200px' : '0px';
-    isShow = !isShow;
-} );
